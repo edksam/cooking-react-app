2e0e0137c4d3c44a53dfd770675d06f2482c5463
edksam <paakofisam.sam>
2020-10-05 10:56:53 +0100

    Initial Commit

---
+++ src/CounterHooks.js
@@ -0,0 +1,27 @@
+import React, { useState, useContext } from "react";
+import { ThemeContext } from "./App";
+
+function CounterHooks({ initialCount }) {
+  const [count, setCount] = useState(initialCount);
+  const style = useContext(ThemeContext);
+  return (
+    <div>
+      <div>
+        <button
+          style={style}
+          onClick={() => setCount((prevCount) => prevCount - 1)}
+        >
+          -
+        </button>
+        <span>{count}</span>
+        <button
+          style={style}
+          onClick={() => setCount((prevState) => prevState + 1)}
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
+export default CounterHooks;
